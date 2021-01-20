3906b8a163dfd1defbadd3a5e12c19cd5bfd7d4a
aorlander <49505752+aorlander@users.noreply.github.com>
2021-01-19 23:58:50 -0500

    basics working for sha256 -- hexidigest and looping through k

--- hash_collision.py
+++ hash_collision.py
@@ -1,12 +1,10 @@
 import hashlib
 import os
 
-# Write a function called “hash_collision” that takes a single input, k, where k is an integer. 
-# The function “hash_collision” should return two variables, x and y, such that that the SHA256(x) and SHA256(y) match on their final k bits. 
-# Your algorithm should be randomized, i.e., hash_collision(k) should not always return the same colliding pair.
-
 def hash_collision(k):
- 
+    
+    from hashlib import sha256
+
     if not isinstance(k,int):
         print( "hash_collision expects an integer" )
         return( b'\x00',b'\x00' )
@@ -17,21 +15,26 @@ def hash_collision(k):
     #Collision finding code goes here
     x = b'\x00'
     y = b'\x00'
-
+    
     #The return variables, x and y, should be encoded as bytes.
     #To encode a string as bytes
-    str = "Hello World"
-    byte_str = str.encode('utf-8')
-    hsh = hashlib.sha256().update(byte_str)
-    print(hsh)
+    str1 = "HELLO"
+    byte_str1 = str1.encode('utf-8')
 
-    
-    print(x, y)
-
+    h_x = sha256(x).hexdigest()
+    h_y = sha256(y).hexdigest()
+    for j in range(k):
+        #print(j)
+        #print("h_x=" + h_x[j])
+        #print("h_y=" + h_y[j])
+        #print("======")
+        if(h_x[j]!=h_y[j]):
+            break
+                
     return( x, y )
     pass
 
 
 # The python interpreter actually executes the function body here
 print("Answer: ")
-hash_collision(3)
+print(hash_collision(3))
