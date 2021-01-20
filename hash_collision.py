a6dee561f28f373c7997143b144a051a4e6e73bd
aorlander <49505752+aorlander@users.noreply.github.com>
2021-01-20 01:13:50 -0500

    repeat until finding a collision

--- hash_collision.py
+++ hash_collision.py
@@ -15,46 +15,48 @@ def hash_collision(k):
         return( b'\x00',b'\x00' )
     
     #Collision finding code goes here
-    
+    # n bits long digest is at most as secure as a symmetric 
+    # encryption algorithm keyed with n/2 bits
     upper_bound = round(pow(2,k),0)
-    #The return variables, x and y, should be encoded as bytes.
-    #To encode a string as bytes
     seed = str(randrange(upper_bound))
     byte_seed = seed.encode('utf-8')
     x = byte_seed
     y = byte_seed * 2
-    
-    for i in range(upper_bound):
-        h_x = sha256(x).hexdigest()
-        h_y = sha256(y).hexdigest()
-        #You're checking the last k bits of the hash produced 
-        # by the sha256. You'll have to convert the hex number 
-        # into binary and look at the last k digits.
-        b_x = "{0:08b}".format(int(h_x, 16)) 
-        b_y = "{0:08b}".format(int(h_y, 16))
+    flag=0
 
-        if ((b_x)[-k:] == (b_y)[-k:]):
-            print("MATCH!")
-            print("--------------H(x)-----------------")
-            print((b_x))
-            print("--------------H(y)-----------------")
-            print((b_y))
-            print("-------------------------------")
-            print("Last " + str(k) + " digits") 
-            print("-------------------------------")
-            print((b_x)[-k:])
-            print((b_y)[-k:]) 
-            break
-        else:
-            x = int(x)
-            y = int(y)
-            x=x+randrange(upper_bound)
-            y=y+randrange(upper_bound)
-            x = str(x).encode('utf-8')
-            y = str(y).encode('utf-8')
+    while(flag==0):
+        for i in range(upper_bound):
+            h_x = sha256(x).hexdigest()
+            h_y = sha256(y).hexdigest()
+            #You're checking the last k bits of the hash produced 
+            # by the sha256. You'll have to convert the hex number 
+            # into binary and look at the last k digits.
+            b_x = "{0:08b}".format(int(h_x, 16)) 
+            b_y = "{0:08b}".format(int(h_y, 16))
 
-    # n bits long digest is at most as secure as a symmetric 
-    # encryption algorithm keyed with n/2 bits
+            if ((b_x)[-k:] == (b_y)[-k:]):
+                #print("MATCH!")
+                #print("--------------H(x)-----------------")
+                #print((b_x))
+                #print("--------------H(y)-----------------")
+                #print((b_y))
+                #print("-------------------------------")
+                #print("Last " + str(k) + " digits") 
+                #print("-------------------------------")
+                #print((b_x)[-k:])
+                #print((b_y)[-k:]) 
+                flag=1
+                break
+            else:
+                x = int(x)
+                y = int(y)
+                x=x+randrange(upper_bound)
+                y=y+randrange(upper_bound)
+                x = str(x).encode('utf-8')
+                y = str(y).encode('utf-8')
+        
+
+    #The return variables, x and y, should be encoded as bytes.
     x=bytes(x)
     y=bytes(y)
     return( x, y )
